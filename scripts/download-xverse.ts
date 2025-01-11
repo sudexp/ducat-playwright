7e62491a79805a7c7c059ec0b982ec022212a365
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2025-01-11 21:55:46 +0200

    download-xverse script corrected

--- scripts/download-xverse.ts
+++ scripts/download-xverse.ts
@@ -2,21 +2,30 @@ import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 import axios from 'axios';
-import unzipCrx from 'unzip-crx'; // Импортируем unzip-crx
+import crx from 'crx-util';
 
 dotenv.config();
 
-const CRX_URL = process.env.CRX_URL as string;
-const EXTENSION_DIR = path.join(__dirname, '../xverse');
+const CRX_URL = process.env.CRX_URL;
 
 if (!CRX_URL) {
   console.error('CRX_URL is not defined in .env file!');
   process.exit(1);
 }
 
+const EXTENSION_DIR = path.join(__dirname, '../xverse');
+const CRX_FILE_PATH = path.join(__dirname, 'xverse.crx');
+
+// function to load CRX
 const downloadFile = async (url: string, destination: string): Promise<void> => {
   const writer = fs.createWriteStream(destination);
-  const { data } = await axios.get(url, { responseType: 'stream' });
+  const { data } = await axios.get(url, {
+    responseType: 'stream',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+      Accept: '*/*',
+    },
+  });
 
   data.pipe(writer);
 
@@ -26,22 +35,26 @@ const downloadFile = async (url: string, destination: string): Promise<void> => 
   });
 };
 
+// create directory if not present
 if (!fs.existsSync(EXTENSION_DIR)) {
   fs.mkdirSync(EXTENSION_DIR, { recursive: true });
 }
 
+// download and unpack
 (async () => {
   try {
-    const crxFilePath = path.join(__dirname, 'xverse.crx');
+    console.log('📥 Downloading Xverse extension...');
 
-    console.log('Downloading Xverse extension...');
-    await downloadFile(CRX_URL, crxFilePath);
+    await downloadFile(CRX_URL, CRX_FILE_PATH);
+    console.log('✅ Xverse extension downloaded successfully at:', CRX_FILE_PATH);
+    console.log('📦 Unpacking CRX extension...');
 
-    console.log('Unzipping CRX extension...');
-    await unzipCrx(crxFilePath, EXTENSION_DIR);
+    await crx.parser.extract(CRX_FILE_PATH, EXTENSION_DIR);
+    console.log('✅ Xverse extension unpacked successfully at:', EXTENSION_DIR);
 
-    console.log('Xverse extension downloaded and unpacked successfully!');
+    fs.unlinkSync(CRX_FILE_PATH);
+    console.log('✅ The CRX file has been delelted', CRX_FILE_PATH);
   } catch (error) {
-    console.error('Error downloading or unpacking the extension:', error);
+    console.error('❌ Error downloading or unpacking the extension:', error);
   }
 })();
