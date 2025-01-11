b5ff307d9e8a5007f6590e51c6657ba8fd368f22
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2025-01-11 12:28:43 +0200

    test improved

--- tests/launch-app.spec.ts
+++ tests/launch-app.spec.ts
@@ -1,16 +1,27 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  if (!process.env.URL) {
-    throw new Error('Environment variable URL is not defined.');
+  const ROOT_URL = process.env.ROOT_URL;
+
+  if (!ROOT_URL) {
+    throw new Error('ROOT_URL is not defined.');
   }
 
-  await page.goto(process.env.URL);
+  await page.goto(ROOT_URL);
 });
 
-test('Launch App button click', async ({ page }) => {
+test('Launch App button click opens new page', async ({ page }) => {
   const launchButton = page.locator('p.framer-text', { hasText: 'Launch App' }).first();
 
   await expect(launchButton).toBeVisible();
-  await launchButton.click();
+
+  const [newPage] = await Promise.all([page.context().waitForEvent('page'), launchButton.click()]);
+
+  const EARLY_SUCCESS_URL = process.env.EARLY_SUCCESS_URL;
+
+  if (!EARLY_SUCCESS_URL) {
+    throw new Error('EARLY_SUCCESS_URL is not defined');
+  }
+
+  await expect(newPage).toHaveURL(EARLY_SUCCESS_URL);
 });
