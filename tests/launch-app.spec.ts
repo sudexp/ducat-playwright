947c3a2e109e6d00618e58d52165a4537d833fa6
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2025-01-11 14:04:28 +0200

    clickLaunchAppAndWaitForPage to utils

--- tests/launch-app.spec.ts
+++ tests/launch-app.spec.ts
@@ -1,21 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-const clickLaunchAppAndWaitForPage = async (page: any) => {
-  const launchButton = page.locator('p.framer-text', { hasText: 'Launch App' }).first();
-
-  await expect(launchButton).toBeVisible();
-
-  const [newPage] = await Promise.all([page.context().waitForEvent('page'), launchButton.click()]);
-  const EARLY_SUCCESS_URL = process.env.EARLY_SUCCESS_URL;
-
-  if (!EARLY_SUCCESS_URL) {
-    throw new Error('EARLY_SUCCESS_URL is not defined');
-  }
-
-  await expect(newPage).toHaveURL(EARLY_SUCCESS_URL);
-
-  return newPage;
-};
+import { clickLaunchAppAndWaitForPage } from '../utils/launch-app';
 
 test.beforeEach(async ({ page }) => {
   const ROOT_URL = process.env.ROOT_URL;
---
+++ utils/launch-app.ts
@@ -0,0 +1,18 @@
+import { expect } from '@playwright/test';
+
+export const clickLaunchAppAndWaitForPage = async (page: any) => {
+  const launchButton = page.locator('p.framer-text', { hasText: 'Launch App' }).first();
+
+  await expect(launchButton).toBeVisible();
+
+  const [newPage] = await Promise.all([page.context().waitForEvent('page'), launchButton.click()]);
+  const EARLY_SUCCESS_URL = process.env.EARLY_SUCCESS_URL;
+
+  if (!EARLY_SUCCESS_URL) {
+    throw new Error('EARLY_SUCCESS_URL is not defined');
+  }
+
+  await expect(newPage).toHaveURL(EARLY_SUCCESS_URL);
+
+  return newPage;
+};
