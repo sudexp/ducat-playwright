49f279165b41505ebed77fa2c0d3a701101d3930
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2025-01-11 15:04:43 +0200

    "Connect Wallet button click shows temporarily popup if Xverse Wallet is not installed" test

--- tests/launch-app.spec.ts
+++ tests/launch-app.spec.ts
@@ -23,11 +23,26 @@ test('Launch App button click opens new page', async ({ page }) => {
   await expect(newPage).toHaveURL(EARLY_SUCCESS_URL);
 });
 
-test('Connect Wallet button click', async ({ page }) => {
+test('Connect Wallet button click shows temporarily popup if Xverse Wallet is not installed', async ({ page }) => {
   const newPage = await clickLaunchAppAndWaitForPage(page);
   const connectWalletButton = newPage.locator('button', { hasText: 'Connect Wallet' }).first();
 
   await connectWalletButton.waitFor({ state: 'visible' /*, timeout: 1000 */ });
   await expect(connectWalletButton).toBeVisible();
   await connectWalletButton.click();
+
+  const popupText = 'Please Install Xverse Wallet to Continue.';
+
+  const toastPopup = newPage.locator('section[aria-label="Notifications alt+T"] div').filter({ hasText: popupText });
+
+  try {
+    await toastPopup.waitFor({ state: 'visible', timeout: 2000 });
+    await expect(toastPopup).toBeVisible();
+    await expect(toastPopup).toHaveText(popupText);
+
+    await toastPopup.waitFor({ state: 'detached', timeout: 7000 });
+    await expect(toastPopup).not.toBeVisible();
+  } catch (err) {
+    console.error('Toast popup did not appear or detached: ', err);
+  }
 });
