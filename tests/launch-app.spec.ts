03b3da7885415a15da09027e618748b205fc5881
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2025-01-11 13:41:46 +0200

    second test created

--- tests/launch-app.spec.ts
+++ tests/launch-app.spec.ts
@@ -16,7 +16,6 @@ test('Launch App button click opens new page', async ({ page }) => {
   await expect(launchButton).toBeVisible();
 
   const [newPage] = await Promise.all([page.context().waitForEvent('page'), launchButton.click()]);
-
   const EARLY_SUCCESS_URL = process.env.EARLY_SUCCESS_URL;
 
   if (!EARLY_SUCCESS_URL) {
@@ -24,4 +23,23 @@ test('Launch App button click opens new page', async ({ page }) => {
   }
 
   await expect(newPage).toHaveURL(EARLY_SUCCESS_URL);
+});
+
+// TODO: reuse code from the first test
+test('Connect Wallet button click', async ({ page }) => {
+  const launchButton = page.locator('p.framer-text', { hasText: 'Launch App' }).first();
+  const [newPage] = await Promise.all([page.context().waitForEvent('page'), launchButton.click()]);
+
+  const EARLY_SUCCESS_URL = process.env.EARLY_SUCCESS_URL;
+  if (!EARLY_SUCCESS_URL) {
+    throw new Error('EARLY_SUCCESS_URL is not defined');
+  }
+  await expect(newPage).toHaveURL(EARLY_SUCCESS_URL);
+
+  const connectWalletButton = newPage.locator('button', { hasText: 'Connect Wallet' }).first();
+
+  await connectWalletButton.waitFor({ state: 'visible', timeout: 2000 });
+
+  await expect(connectWalletButton).toBeVisible();
+  await connectWalletButton.click();
 });
